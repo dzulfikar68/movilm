4a3daa7dbc52a9dd20477d986a3ee033b4ef7d9b
dzulfikar68 <dzulfikar68.fauzi@gmail.com>
2023-07-23 11:15:53 +0700

    first push

---
+++ js/model.js
@@ -0,0 +1,9 @@
+// class Response {
+//     static from(json){
+//         return Object.assign(new Response(), json);
+//     }
+//     constructor() {
+//         this.original_title = "";
+//         this.tagline = "";
+//     }
+// }
